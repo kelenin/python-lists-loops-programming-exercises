b0c31b4e21e5c5411da37b7d5347771bbbf57b22
Katheren Salom <katherensalom@gmail.com>
2022-07-19 23:20:39 +0000

    new file:   .learn/resets/01-hello-world/app.py new file:   .learn/resets/01.1-Access-and-Retrieve/app.py new file:   .learn/resets/01.2-Retrieve-items/app.py new file:   .learn/resets/01.3-Print-the-last-one/app.py new file:   .learn/resets/01.4-Add-item-to-list/app.py new file:   .learn/resets/01.5-loop-seventeen/app.py new file:   .learn/resets/02-Loop-list/app.py new file:   .learn/resets/02.1-Loop-from-the-top/app.py new file:   exercises/01-hello-world/__pycache__/test.cpython-38-PYTEST.pyc modified:   exercises/01-hello-world/app.py new file:   exercises/01.1-Access-and-Retrieve/__pycache__/app.cpython-38.pyc new file:   exercises/01.1-Access-and-Retrieve/__pycache__/test.cpython-38-PYTEST.pyc modified:   exercises/01.1-Access-and-Retrieve/app.py new file:   exercises/01.2-Retrieve-items/__pycache__/test.cpython-38-PYTEST.pyc modified:   exercises/01.2-Retrieve-items/app.py new file:   exercises/01.3-Print-the-last-one/__pycache__/app.cpython-38.pyc new file:   exercises/01.3-Print-the-last-one/__pycache__/test.cpython-38-PYTEST.pyc modified:   exercises/01.3-Print-the-last-one/app.py new file:   exercises/01.4-Add-item-to-list/__pycache__/app.cpython-38.pyc new file:   exercises/01.4-Add-item-to-list/__pycache__/test.cpython-38-PYTEST.pyc modified:   exercises/01.4-Add-item-to-list/app.py new file:   exercises/01.5-loop-seventeen/__pycache__/test.cpython-38-PYTEST.pyc modified:   exercises/01.5-loop-seventeen/app.py new file:   exercises/02-Loop-list/__pycache__/test.cpython-38-PYTEST.pyc modified:   exercises/02-Loop-list/app.py

--- exercises/01.4-Add-item-to-list/app.py
+++ exercises/01.4-Add-item-to-list/app.py
@@ -1,5 +1,10 @@
 #Remember import random function here:
+import random
 
 my_list = [4,5,734,43,45]
 
 #The magic is here:
+for i in range(5,15):
+ n = random.randint(6,80)
+ my_list.append(n)
+print(my_list)
